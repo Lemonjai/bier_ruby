bd5f9b826098c7b13a487a3f24876c0b5c4aa7fb
lemonjai <leocheung_1986@hotmail.com>
2016-09-17 17:20:13 -0400

    added post nad models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'brands/index'
+  resources :beers
 
-  root 'main#index'
+  root 'beers#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
